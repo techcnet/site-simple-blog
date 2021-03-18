77e657bab284edf52b43e4eea5b7d893dd24aaf9
techcnet <70081687+techcnet@users.noreply.github.com>
2021-03-18 08:01:37 +0700

    Add files via upload

---
+++ site-simple-blog.info.php
@@ -0,0 +1,7 @@
+<?php namespace ProcessWire;
+$info = array(
+  'title' => 'Simple Blog Profile for ProcessWire 3x',
+  'summary' => 'This site profile for ProcessWire offers a simple and responsive blog which doesn\'t require any third-party modules. It works exclusively based on some template files.',
+  'author' => 'tech-c.net',
+  'version' => 101
+);
