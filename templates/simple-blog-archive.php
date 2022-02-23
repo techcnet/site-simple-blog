67780ff2e35acf7b29d45e682fec5405f0a761eb
techcnet <70081687+techcnet@users.noreply.github.com>
2022-02-23 08:34:12 +0700

    Delete templates directory

--- templates/simple-blog-archive.php
+++
@@ -1,2 +0,0 @@
-<?php namespace ProcessWire;
-  $content .= simple_blog_get_archive($pages, $user->language);
--- templates/simple-blog-category.php
+++
@@ -1,2 +0,0 @@
-<?php namespace ProcessWire;
-  $content .= simple_blog_get_posts($pages, $user->language, $page->title, 0, 10);
