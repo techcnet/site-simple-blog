45ac5be3ce09aa793862015ffd498d9a33a3fede
techcnet <70081687+techcnet@users.noreply.github.com>
2023-12-28 14:36:05 +0700

    Delete install/info.php

--- install/info.php
+++
@@ -1,7 +0,0 @@
-<?php namespace ProcessWire;
-if(!defined("PROCESSWIRE_INSTALL")) die();
-$info = array(
-	'title' => "Simple Blog Profile for ProcessWire 3x", 
-	'summary' => "This site profile for ProcessWire offers a simple and responsive blog which doesn't require any module. It works exclusively with some template files.", 
-	'screenshot' => "screenshot.jpg"
-);
