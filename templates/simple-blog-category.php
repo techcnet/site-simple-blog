2943290aea2bbab400c4ae150c34c545fc47d231
techcnet <70081687+techcnet@users.noreply.github.com>
2023-12-28 14:31:25 +0700

    Delete templates/simple-blog-category.php

--- templates/simple-blog-category.php
+++
@@ -1,4 +0,0 @@
-<?php namespace ProcessWire;
-  $content = $page->body;
-  include_once('./_simple-blog-func.php');
-  $content .= simple_blog_get_categories($pages, $user->language, $page->title, 0, 0, 1, 10);
